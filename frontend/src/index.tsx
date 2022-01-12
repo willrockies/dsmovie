8b37cea05835d854624288989d1671d598419117
willrockies <wlsn.ads@gmail.com>
2022-01-12 20:39:05 -0300

    bootstrap

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,8 +1,10 @@
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
